559d7bf51d81c2ab2a365e7a4d542f4c85e88d02
brady.ouren <brady.ouren@gmail.com>
2016-10-05 11:40:41 -0700

    get dry run working

--- executable/Main.hs
+++ executable/Main.hs
@@ -1,8 +1,5 @@
 module Main (main) where
 
 import qualified Media.Split
--- import System.Remote.Monitoring
 
-main :: IO ()
 main = Media.Split.main
-  -- ekg <- forkServer "localhost" 8000
--- src/Media/Split.hs
+++ src/Media/Split.hs
@@ -1,29 +1,76 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
 module Media.Split where
 
 import System.Process
-import System.FilePath (takeExtension)
+import System.FilePath
 import System.Directory
-import qualified  Data.Text as T
+import System.IO.Unsafe
+import Data.Monoid ((<>))
+import Options.Generic
+import Data.List (intercalate, groupBy)
+import Data.Char
+import Data.Maybe (fromJust)
+import qualified Data.Map.Lazy as M
+import GHC.IO.Exception (ExitCode)
+import System.Exit (exitSuccess, exitFailure)
 
-bookExtensions = [".djvu", ".azw3", ".epub", ".mobi", ".pdf"]
+type RemoteFilePath = String
+
+data Manifest
+  = Manifest
+  { target :: FilePath
+  , book_dest :: Maybe String
+  , music_dest :: Maybe String
+  , dry_run :: Bool
+  } deriving (Generic, Show)
+
+instance ParseRecord Manifest
+
+data Media
+  = Book
+  | Music
+  deriving (Show, Eq, Ord)
+
 videoExtensions = []
-musicExtensions = [".mp3", "mp4", ".wav", ".flac", ".ogg", ".aiff"]
+bookExtensions = [".djvu", ".azw3", ".epub", ".mobi", ".pdf"]
+musicExtensions = [".mp3", "mp4", ".wav", ".flac", ".ogg", ".aiff", "aac"]
 
-downcase = T.toLower
+extensionFrom = map toLower . takeExtension
 
-extensionFrom :: FilePath -> T.Text
-extensionFrom = downcase . T.pack . takeExtension
-
-type RemoteFilePath = T.Text
-
-moveFiles :: [FilePath] -> RemoteFilePath -> IO ()
-moveFiles [] _ = log "no files"
-moveFiles fs rfp = do
-  _ <- process $  "scp " ++ fs ++ " " ++ rfp
+moveFiles :: [FilePath] -> Maybe String -> IO ()
+moveFiles [] _ = print "no files" >> pure () -- exitFailure
+moveFiles _ Nothing  = print "no destination" >> pure () -- exitFailure
+moveFiles fs (Just rfp) = do
+  let cmd = "scp -r \"" ++ intercalate " " (fmap (\f -> "\"" ++f++ "\"") fs) ++ " " ++ rfp
+  print cmd
   pure ()
+  -- exitSuccess
+  -- ecode <- system cmd
+  -- return ecode
+
+split :: FilePath -> M.Map Media [FilePath]
+split source = do
+  let fs = unsafePerformIO $ getDirectoryContents source
+
+  M.fromList
+    [ (Book, filter (\fp -> extensionFrom fp `elem` bookExtensions) fs)
+    , (Music, filter (\fp -> extensionFrom fp `elem` musicExtensions) fs)
+    ]
+
+writeManifest :: Manifest -> IO ()
+writeManifest manifest = undefined
+
+run m = do
+  let sm = split $ target m
+  let books = fmap (\fp -> target m </> fp) $ fromJust $ M.lookup Book sm
+  let music = fromJust $ M.lookup Music sm
+  moveFiles books $ music_dest m
+  moveFiles books $ book_dest m
+  return ()
 
 main = do
-  fs <- listDirectory <$> source
-  pure ()
-  -- mapConcurrently moveFiles
+  x <- getRecord "split and move files"
+  print (x :: Manifest)
+
+  run (x :: Manifest)
